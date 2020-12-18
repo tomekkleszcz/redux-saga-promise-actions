19ad5b341eb464ec7e7eafc0a26fad581bfa9f7e
defabricated <defabricated@gmail.com>
2020-12-18 16:05:28 +0100

    Add lint stage; Fix typing for resolvePromiseAction and rejectPromiseAction

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 96,
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    singleQuote: true,
+    trailingComma: "none",
+    tabWidth: 4,
+    arrowParens: "avoid",
+};
