dee06b717c456b70fdcf9a6cd70cb4000082c026
Tomek Kleszcz <4775112+tomekkleszcz@users.noreply.github.com>
2020-12-21 18:18:49 +0100

    Implement new simplified action creator (#17)
    * Implement new simplified action creator
    
    * Update README.md
    
    * Add LICENSE file

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,49 @@
+//Action creators
+import {createPromiseAction} from '../src';
+
 //Helpers
 import {dispatch} from '../src';
 
 //Actions
 import * as actions from './actions';
+
+describe('action creators', () => {
+    describe('simple', () => {
+        const action = createPromiseAction('TEST_ACTION')<undefined, undefined, undefined>();
+
+        it('should have correct request action type', () => {
+            expect(action.request().type).toEqual('TEST_ACTION_REQUEST');
+        });
+
+        it('should have correct success action type', () => {
+            expect(action.success().type).toEqual('TEST_ACTION_SUCCESS');
+        });
+
+        it('should have correct failure action type', () => {
+            expect(action.failure().type).toEqual('TEST_ACTION_FAILURE');
+        });
+    });
+
+    describe('advanced', () => {
+        const action = createPromiseAction(
+            'TEST_ACTION_REQUEST',
+            'TEST_ACTION_SUCCESS',
+            'TEST_ACTION_FAILURE'
+        )<undefined, undefined, undefined>();
+
+        it('should have correct request action type', () => {
+            expect(action.request().type).toEqual('TEST_ACTION_REQUEST');
+        });
+
+        it('should have correct success action type', () => {
+            expect(action.success().type).toEqual('TEST_ACTION_SUCCESS');
+        });
+
+        it('should have correct failure action type', () => {
+            expect(action.failure().type).toEqual('TEST_ACTION_FAILURE');
+        });
+    });
+});
 
 describe('dispatch helper', () => {
     it('should return putResolve if the action is a promise action', () => {
